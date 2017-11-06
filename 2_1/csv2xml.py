2c598f48a0d1e93825fbef41bfde635f923db131
TGR <tuomas.granlund@gmail.com>
2017-11-06 22:33:57 +0200

    Added support for script arguments

--- 2_1/csv2xml.py
+++ 2_1/csv2xml.py
@@ -1,40 +1,58 @@
 import pandas
 import codecs
+import sys
 from bs4 import BeautifulSoup
+
 
 # Author: tuomas.granlund@solita.fi
 # 06.11.2017
 
-# read data from csv and insert column names
-populationData = pandas.read_csv('population.csv', delimiter=';', skiprows=4,
-                                 names=['name', 'year', 'total', 'males', 'females'])
+def read_csv_and_create_xml(input_file_name, output_file_name):
 
-# create object for xml
-soup = BeautifulSoup(features='xml')
-root = soup.new_tag("populationdata")
-soup.append(root)
+    # validate function arguments
+    if input_file_name:
+        csv_file = input_file_name
+    else:
+        csv_file = 'population.csv'
+    if output_file_name:
+        xml_file = output_file_name
+    else:
+        xml_file = "population.xml"
 
-# iterate over data and populate xml object
-for index, row in populationData.iterrows():
-    municipalityTag = soup.new_tag("municipality")
-    nameTag = soup.new_tag("name")
-    nameTag.string = row['name']
-    municipalityTag.append(nameTag)
-    yearTag = soup.new_tag("year")
-    yearTag.string = str(row['year'])
-    municipalityTag.append(yearTag)
-    totalTag = soup.new_tag("total")
-    totalTag.string = str(row['total'])
-    municipalityTag.append(totalTag)
-    malesTag = soup.new_tag("males")
-    malesTag.string = str(row['males'])
-    municipalityTag.append(malesTag)
-    femalesTag = soup.new_tag("females")
-    femalesTag.string = str(row['females'])
-    municipalityTag.append(femalesTag)
-    root.append(municipalityTag)
+    # read data from csv and insert column names
+    population_data = pandas.read_csv(csv_file, delimiter=';', skiprows=4,
+                                      names=['name', 'year', 'total', 'males', 'females'])
 
-# create file and write
-file = codecs.open("population.xml", "w", "utf-8")
-file.write(soup.prettify())
-file.close()
+    # create object for xml
+    soup = BeautifulSoup(features='xml')
+    root = soup.new_tag("populationdata")
+    soup.append(root)
+
+    # iterate over data and populate xml object
+    for index, row in population_data.iterrows():
+        municipality_tag = soup.new_tag("municipality")
+        name_tag = soup.new_tag("name")
+        name_tag.string = row['name']
+        municipality_tag.append(name_tag)
+        year_tag = soup.new_tag("year")
+        year_tag.string = str(row['year'])
+        municipality_tag.append(year_tag)
+        total_tag = soup.new_tag("total")
+        total_tag.string = str(row['total'])
+        municipality_tag.append(total_tag)
+        males_tag = soup.new_tag("males")
+        males_tag.string = str(row['males'])
+        municipality_tag.append(males_tag)
+        females_tag = soup.new_tag("females")
+        females_tag.string = str(row['females'])
+        municipality_tag.append(females_tag)
+        root.append(municipality_tag)
+
+    # create file and write
+    file = codecs.open(xml_file, "w", "utf-8")
+    file.write(soup.prettify())
+    file.close()
+
+
+if __name__ == '__main__':
+    read_csv_and_create_xml(str(sys.argv[1]), str(sys.argv[2]))
