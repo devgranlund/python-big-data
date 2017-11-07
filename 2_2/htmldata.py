1c81514147f8a732c5b329de7807215d915c7a62
TGR <tuomas.granlund@gmail.com>
2017-11-08 00:02:16 +0200

    2.2 second DataFrame done

--- 2_2/htmldata.py
+++ 2_2/htmldata.py
@@ -5,6 +5,7 @@ import pandas as pd
 # Author: tuomas.granlund@solita.fi
 # 07.11.2017
 
+# GDP_(PPP)
 url = "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(PPP)_per_capita"
 html = urlopen(url)
 soup = BeautifulSoup(html, "lxml")
@@ -21,5 +22,16 @@ for i in range(len(data_rows)):
 
 df_gdp = pd.DataFrame(gdp, index=countries, columns=['GDP (PPP) per capita'])
 
+# Happiness score
+hs = pd.read_html("https://en.wikipedia.org/wiki/World_Happiness_Report")
+hs = hs.pop(0)
+hs.drop(hs.columns[[0, 1, 4, 5, 6, 7, 8, 9, 10, 11]], axis=1, inplace=True)
+hs = hs.iloc[1:]
+hs.set_index(2, inplace=True)
 
-print(df_gdp)
+# Merge gdp and happiness
+df_gdp_happiness = pd.merge(df_gdp, hs, left_index=True, right_index=True)
+df_gdp_happiness.rename(columns={3: 'Happiness score'}, inplace=True)
+
+print(df_gdp_happiness)
+
