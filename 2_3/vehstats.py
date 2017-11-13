27438a8cfe723f07aae3a6f1982ea0e56d288c4c
devgranlund <tuomas.granlund@gmail.com>
2017-11-13 23:37:08 +0200

    First version of diagram picture added

--- 2_3/vehstats.py
+++ 2_3/vehstats.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+from matplotlib.gridspec import GridSpec
 
 # Author: tuomas.granlund@solita.fi
 # 12.11.2017
@@ -44,9 +45,11 @@ others_pr = (others_count / count_of_all_cars) * 100
 
 labels = [car_1, car_2, car_3, car_4, car_5, others]
 sizes = [car_1_pr, car_2_pr, car_3_pr, car_4_pr, car_5_pr, others_pr]
-fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4)
-ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-ax1.axis('equal')
+
+the_grid = GridSpec(2, 2)
+plt.subplot(the_grid[0, 0], aspect=1)
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
 
 # The shares of cars by travel distance
 cat_1 = '0-50000 km'
@@ -73,8 +76,9 @@ cat_7_pr = (cat_6_count / count_of_all_cars) * 100
 
 labels = [cat_1, cat_2, cat_3, cat_4, cat_5, cat_6, cat_7]
 sizes = [cat_1_pr, cat_2_pr, cat_3_pr, cat_4_pr, cat_5_pr, cat_6_pr, cat_7_pr]
-ax2.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-ax2.axis('equal')
+plt.subplot(the_grid[0, 1], aspect=1)
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
 
 # How old are the cars?
 df['ensirekisterointipvm'] = pd.to_datetime(df['ensirekisterointipvm'], format="%Y-%m-%d")
@@ -94,8 +98,9 @@ xxl_old_count_pr = (xxl_old_count / count_of_all_cars) * 100
 labels = ['under 5 y old', '5 - 10 y old', '10 - 15 y old',
           '15 - 20 y old', 'over 20 y old']
 sizes = [s_old_count_pr, m_old_count_pr, l_old_count_pr, xl_old_count_pr, xxl_old_count_pr]
-ax3.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-ax3.axis('equal')
+plt.subplot(the_grid[1, 0], aspect=1)
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
 
 # CO2 emissions
 co_1 = len(df[(df['Co2'] <= 100)].index)
@@ -118,10 +123,14 @@ co_8_pr = (co_8 / count_of_all_cars) * 100
 labels = ['under 100 g/km', '100 - 125 g/km', '125 - 150 g/km', '150 - 175 g/km', '175 - 200 g/km',
           '200 - 225 g/km', '225 - 250 g/km', 'over 250 g/km']
 sizes = [co_1_pr, co_2_pr, co_3_pr, co_4_pr, co_5_pr, co_6_pr, co_7_pr, co_8_pr]
-ax4.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-ax4.axis('equal')
+plt.subplot(the_grid[1, 1])
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+plt.axis('equal')
 
-plt.show()
+# Create the picture
+plt.subplots_adjust(top=0.98, bottom=0.02)
+plt.tight_layout()
+plt.savefig('vehstats.png')
 
 # veh_data = veh_data.loc[veh_data['ajoneuvoluokka'].isin('M1', 'M1G')]
 
